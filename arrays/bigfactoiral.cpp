eb1d686ed8838ec4ab012651be5b1a8ac5c8c509
MohamedFawzy <mfawzy22@gmail.com>
2020-06-04 16:28:11 +0400

    fix changes

--- arrays/bigfactoiral.cpp
+++ arrays/bigfactoiral.cpp
@@ -3,37 +3,43 @@ using namespace std;
 #define MAX 500
 //https://www.geeksforgeeks.org/factorial-large-number/
 
-int multiply(int x, int res[], int res_size);
+void factorial(int n);
+int multiply(int x, int ar[], int ar_size);
 
 void factorial(int n)
 {
-  int res[MAX];
-  res[0] = 1;
-  int res_size = 1;
-  for(int i=2; i<=n ; i++){
-    res_size = multiply(i, res, res_size);
+
+  int ar[MAX];
+  ar[0]=1;
+  int ar_size = 1;
+  for(int i=2; i<=n; i++){
+    ar_size = multiply(i, ar, ar_size);
   }
-  cout << "factorial of give number " << n <<"\t is given size \t"<< res[1] << "\n";
-  for(int i=res_size-1; i>=0; i--){
-    cout << res[i];
+
+  // print the result
+  cout << "facotrial for given number " << n << " is the following \n";
+  for(int i=ar_size-1; i>=0; i--){
+    cout << ar[i];
   }
+
 }
 
-int multiply(int i, int res[], int res_size)
+int multiply(int x, int ar[], int ar_size)
 {
+
   int carry = 0;
-  for(int i=0; i<res_size; i++){
-    int prod = res[i] * i + carry;
-    res[i] = prod % 10; // store the last digit of prod in res[i]
-    carry = prod / 10; // put the rest in carry
+  for(int i=0; i<ar_size; i++){
+    int prod = ar[i] * x + carry;
+    ar[i] = prod%10;
+    carry = prod/10;
   }
 
   while(carry){
-    res[res_size] = carry%10;
-    carry = carry/10;
-    res_size++;
+    ar[ar_size] = carry%10;
+    carry = carry / 10;
+    ar_size++;
   }
-  return res_size;
+  return ar_size;
 }
 
 
