57ec4833a20bf1e18fc986d2f8a522f2803df803
MohamedFawzy <mfawzy22@gmail.com>
2020-06-05 11:07:58 +0400

    revert for basic

--- arrays/premuationcycle.cpp
+++ arrays/premuationcycle.cpp
@@ -1,23 +1,25 @@
-#include <bits/stdc++.h>
+#include <iostream>
+/***
+To detect a cycle in an array or a linked list,
+ we can use Floyd’s slow and fast pointers approach.
+
+Algorithm :
+
+  Start two pointers, slow & fast from head or 0th index.
+  move slow pointer one index/node forward.
+  move fast pointer two indexes/nodes forward.
+  if slow & fast pointer’s index/node are same:
+  There is a loop.
+  if two pointers reach the last index/node:
+  the array/linked list has no loops.
+***/
 using namespace std;
 
 void cycles(int arr[], int size);
 
 void cycles(int arr[], int size)
 {
-    // mapping for key, value of the array
-    int i=0;
-    int index = 0;
-    // initialize container
-    multimap<int, int> map;
-    while(i < size){
-      map.insert({i,1});
-      if(map.count(index+1)) cout << "lol\n";
-      cout << "i: " <<  i << "\tarr[i]: " << arr[i]<<endl;
-      cout << "arr[i]: " << arr[i] << "\tindex+1: " << index+1<<endl;
-      i++;
-      index++;
-    }
+
 }
 
 
