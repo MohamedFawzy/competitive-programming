7bac9a57312fc15e42cad327b76a2151ca64b804
MohamedFawzy <mfawzy22@gmail.com>
2020-06-10 22:40:01 +0400

    add algorithm for matrix directions with k steps

--- arrays/magic_square.cpp
+++ arrays/magic_square.cpp
@@ -2,6 +2,41 @@
 using namespace std;
 const int N=3;
 
+enum dir { DOWN=0, RIGHT, UP, LEFT, UP_LEFT, DOWN_RIGHT, UP_RIGHT, DOWN_LEFT };
+int dir_r[] = {1, 0, -1, 0, -1, 1, -1, 1};
+int dir_c[] = {0, 1, 0, -1, -1, 1, 1, -1};
+
+// optimized version using matrix directions
+int sumWithDir(int r, int c, int dir, int steps, int square[N][N])
+{
+  int sum =0;
+  for(int s=0; s<steps; s++){
+    sum += square[r][c];
+    r += dir_r[dir];
+    c += dir_c[dir];
+  }
+
+  return sum;
+}
+
+bool validateMagic(int square[N][N])
+{
+  int val = sumWithDir(0, 0, RIGHT, N, square);
+
+  for(int i=0; i<N; i++){
+    if(val != sumWithDir(i, 0, RIGHT, N, square)) return false;
+    if(val != sumWithDir(0, i, DOWN, N, square)) return false;
+  }
+
+  if(val != sumWithDir(0,0, DOWN_RIGHT, N, square)) return false;
+  if(val != sumWithDir(0, N-1, DOWN_LEFT, N, square)) return false;
+
+  return true;
+}
+
+
+
+
 int sumRow(int i, int square[N][N])
 {
   int sum =0;
@@ -65,11 +100,23 @@ int main()
     cout << "Not Valid\n";
   }
 
+  if(validateMagic(square)){
+    cout << "valid\n";
+  }else{
+    cout << "not valid\n";
+  }
+
   square[0][0]++;
   if(validate(square)){
     cout << "Valid\n";
   }else{
     cout << "Not Valid\n";
+  }
+
+  if(validateMagic(square)){
+    cout << "valid\n";
+  }else{
+    cout << "not valid\n";
   }
 
   return 0;
