c8cabeddfa3358a45c2449c9cd2ba12ab85a69f1
MohamedFawzy <mfawzy22@gmail.com>
2020-06-13 00:31:29 +0400

    init solution with array matrix directions

--- arrays/connect4-game.cpp
+++ arrays/connect4-game.cpp
@@ -18,7 +18,47 @@ int dir_c[] = {0,        1,     0,  -1,   -1,        1,             1,     -1};
 const int N=6;
 void displayBoard(int arr[N][N]);
 void addItem(int arr[N][N], int item);
+int sumWithDir(int r, int c, int dir, int steps, int arr[N][N]);
+void player_wins(int arr[N][N], int sum_player1, int sum_player2);
 
+void player_wins(int arr[N][N], int sum_player1, int sum_player2)
+{
+  for(int i=0; i<N; i++){
+    int sum = sumWithDir(i, 0, RIGHT, 4, arr);
+    cout << "Sum of right:\t" sum<<"\n";
+    if(sum ==4){
+      sum_player1 += 1;
+    }else if(sumWithDir(0, i, DOWN, 4, arr)==8){
+      sum_player2 += 1;
+    }
+  }
+
+
+  if(sumWithDir(0,0, DOWN_RIGHT, 4, arr)==4){
+    sum_player1 +=1;
+  }else if(sumWithDir(0, 0, DOWN_RIGHT, 4, arr)==8){
+    sum_player2 += 1;
+  }
+  if(sumWithDir(0, N-1, DOWN_LEFT, 4, arr)==4){
+    sum_player1 += 1;
+  }else if(sumWithDir(0, N-1, DOWN_LEFT, 4, arr)==8){
+    sum_player2 += 1 ;
+  }
+
+}
+// optimized version using matrix directions
+int sumWithDir(int r, int c, int dir, int steps, int arr[N][N])
+{
+
+  int sum =0;
+  for(int s=0; s<steps; s++){
+    sum += arr[r][c];
+    r += dir_r[dir];
+    c += dir_c[dir];
+  }
+
+  return sum;
+}
 
 void addItem(int arr[N][N], int color)
 {
@@ -84,18 +124,25 @@ int main()
   displayBoard(arr);
   cout << "\n\n\n";
 
-  for(int r=0; r<N; r++){
-    for(int c=0; c<N; c++){
-      // player 1 starts
-      cout << "Player 1 move\n";
-      addItem(arr,player1_color);
-      displayBoard(arr);
-      cout << "Player 2 move\n";
-      addItem(arr, player2_color);
-      displayBoard(arr);
-    }
+  int cnt =0;
+  int player1_wins =  0;
+  int player2_wins = 0;
+
+  while(cnt < N*N){
+    // player 1 starts
+    cout << "Player 1 move\n";
+    addItem(arr,player1_color);
+    displayBoard(arr);
+    // player 2 starts
+    cout << "Player 2 move\n";
+    addItem(arr, player2_color);
+    displayBoard(arr);
+    // sum the player values
+    player_wins(arr, player1_wins, player2_wins);
+    cout << "player1 wins >>>>>>>\t" << player1_wins<<"\n\n";
+    cout << "player2 wins >>>>>>>\t" << player2_wins<<"\n\n";
+    cnt++;
   }
-
-
+  cout << "Done\n";
   return 0;
 }
